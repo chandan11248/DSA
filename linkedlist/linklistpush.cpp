7c784a663ec3c661a8f10f52173253d998dcacb8
chandan shah <letschandansah@gmail.com>
2025-06-10 23:41:56 +0545

    add initial implementation of node class and main function for linked list

---
+++ linkedlist/linklistpush.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node* next;
+
+    node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+
+};
+
+
+int main()
+{
+
+cout<<"enter the data"<<endl;
+
+    return 0;
+}
