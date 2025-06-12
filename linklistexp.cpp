82f5785773f3bd4e73371b9b186f60420b35baef
chandan shah <letschandansah@gmail.com>
2025-06-12 07:06:57 +0545

    add initial implementation of linked list with node and list classes

---
+++ linklistexp.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+ class node 
+ {
+ int data;
+ public:
+ node* next;
+ node(int x){
+data=x;
+ }
+ };
+
+
+ class list {
+
+    node* head;
+    node* tail;
+    public:
+    list()
+{
+    head=tail=0;
+}
+    
+
+ };
+
+int main()
+{
+    return 0;
+
+}
